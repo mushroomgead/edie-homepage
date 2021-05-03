06f1133c72753b8b2e53dc17e1989b93a552f334
Kate <mushroom.gead@gmail.com>
2021-05-03 11:08:50 +0700

    apply theme

--- components/pages/homepage/IntroSection/index.js
+++ components/pages/homepage/IntroSection/index.js
@@ -2,7 +2,7 @@ import ContactButton from '../../../commons/ContactButton'
 
 export default function IntroSection() {
   return (
-    <div className="pt-12 pb-12">
+    <div className="pt-12 pb-12 bg-brand">
       <div className="flex flex-col pl-8 pb-3.5">
         <span className="text-sm text-blue-400">Unhappy with your website?</span>
           <span className="text-2xl">We create beautiful</span>
